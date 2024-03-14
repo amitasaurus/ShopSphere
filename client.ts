4db29dc9b2186e1b08b3f718d9bf932907e1c221
Amit Singh <amit195@hotmail.com>
2024-03-14 08:57:08 +0530

    chore: :closed_lock_with_key: made sanity studio work independently

--- client.ts
+++ client.ts
@@ -3,7 +3,7 @@ import { createClient } from 'next-sanity';
 export default createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  useCdn: true,
   token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
-  apiVersion: 'v2024-03-12',
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  useCdn: true,
 });
